f5e22c4f318b62a19237420fa43b87b0b4daa836
hoangnhat <elosol185@gmail.com>
2025-07-25 11:19:33 +0700

    push lan 1

---
+++ WordCounter/WordCounter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class WordCounter
+{
+    static void Main()
+    {
+        Console.WriteLine("Nhập một chuỗi:");
+        string input = Console.ReadLine();
+
+        int wordCount = CountWords(input);
+        Console.WriteLine($"Số từ trong chuỗi là: {wordCount}");
+    }
+
+    static int CountWords(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            return 0;
+
+        string[] words = text.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
